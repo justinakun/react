812ddad7acf0c41de0e3c75911ddd1f96beb4b80
Justina Kunigonyte <justina.kunigonyte@gmail.com>
2023-05-23 18:36:30 +0300

    changes made to the task according to Rokas

--- 2_paskaita/src/components/Hero.jsx
+++ 2_paskaita/src/components/Hero.jsx
@@ -1,29 +1,13 @@
 import PropTypes from 'prop-types'
 
 const Hero = ({title, subtitle}) => {
-    if(!title && !subtitle){
-        return
-    } else if (!title && subtitle){
         return (
             <div className='heroDiv'>
-                <p>{subtitle}</p>
-            </div>
-        )
-    } else if (!subtitle && title){
-        return (
-            <div className='heroDiv'>
-                <h1>{title}</h1>
-            </div>
-        )    }
-     else {
-        return (
-            <div className='heroDiv'>
-                <h1>{title}</h1>
-                <p>{subtitle}</p>
+                {title && <h1>{title}</h1>}
+                {subtitle && <p>{subtitle}</p>}
             </div>            
         )
     }
-}
 
 Hero.propTypes = {
     title: PropTypes.string,
